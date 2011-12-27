6390b421d20e6ed2d63c6018e4b880271e6cd852
Rahul Bhargava <rahulb@media.mit.edu>
2011-12-27 14:38:49 -0500

    add multi-source scraping

--- lib/news_scrapers.rb
+++ lib/news_scrapers.rb
@@ -15,17 +15,20 @@ module NewsScrapers
   # Main Public API - scrape everything for all dates!
   def self.scrape_all
     dates = []
-    #(Date.new(1979,3,5)..Date.new(1979,3,9)).each { |d| dates << d }
-    #(Date.new(1989,3,6)..Date.new(1989,3,10)).each { |d| dates << d }
-    #(Date.new(1999,3,1)..Date.new(1999,3,5)).each { |d| dates << d }
+    (Date.new(1979,3,5)..Date.new(1979,3,9)).each { |d| dates << d }
+    (Date.new(1989,3,6)..Date.new(1989,3,10)).each { |d| dates << d }
+    (Date.new(1999,3,1)..Date.new(1999,3,5)).each { |d| dates << d }
     (Date.new(2009,3,2)..Date.new(2009,3,6)).each { |d| dates << d }
     NewsScrapers.logger.info "Starting to scrape:"
-    #scraper = NewsScrapers::NewYorkTimesScraper.new
-    scraper = NewsScrapers::WashPoScraper.new
-    #scraper = NewsScrapers::LaTimesScraper.new
+    scrapers = []
+    #scrapers.push( NewsScrapers::NewYorkTimesScraper.new )
+    scrapers.push( NewsScrapers::WashPoScraper.new )
+    scrapers.push( NewsScrapers::LaTimesScraper.new )
     dates.each do |d|
       NewsScrapers.logger.info"  #{d}"
-      scraper.scrape(d)
+      scrapers.each do |scraper|
+        scraper.scrape(d)
+      end
     end
   end
   
