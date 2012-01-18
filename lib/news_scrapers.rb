1ffb7a965edc692ee761fa280a832acf86f4ac85
Rahul Bhargava <rahulb@media.mit.edu>
2012-01-18 09:17:48 -0500

    added rake tasks for scraping from just one source

--- lib/news_scrapers.rb
+++ lib/news_scrapers.rb
@@ -15,33 +15,65 @@ module NewsScrapers
   
   @@cache = nil
   
-  # Main Public API - scrape everything for all dates!
-  def self.scrape_all
+  def self.all_dates
     dates = []
     (Date.new(1979,3,5)..Date.new(1979,3,9)).each { |d| dates << d }
     (Date.new(1989,3,6)..Date.new(1989,3,10)).each { |d| dates << d }
     (Date.new(1999,3,1)..Date.new(1999,3,5)).each { |d| dates << d }
     (Date.new(2009,3,2)..Date.new(2009,3,6)).each { |d| dates << d }
-    NewsScrapers.logger.info "Starting to scrape:"
+    dates    
+  end
+  
+  def self.scrape_washington_post
+    NewsScrapers.logger.info "---------------------------------------------------------------"
+    NewsScrapers.logger.info "Starting to scrape Washingon Post:"
+  end
+
+  def self.scrape_chicago_tribune
+    NewsScrapers.logger.info "---------------------------------------------------------------"
+    NewsScrapers.logger.info "Starting to scrape Chicago Tribune"
+    self.scrape(self.all_dates, [NewsScrapers::ChicagoTribuneScraper.new])
+  end
+
+  def self.scrape_los_angeles_times
+    NewsScrapers.logger.info "---------------------------------------------------------------"
+    self.scrape(self.all_dates, [NewsScrapers::LaTimesScraper.new])
+  end
+
+  def self.scrape_new_york_times
+    NewsScrapers.logger.info "---------------------------------------------------------------"
+    self.scrape(self.all_dates, [NewsScrapers::NewYorkTimesScraper.new])
+  end
+  
+  # Main Public API - scrape everything for all dates!
+  def self.scrape_all
+    NewsScrapers.logger.info "---------------------------------------------------------------"
+    NewsScrapers.logger.info "Starting to scrape all:"
     scrapers = []
     scrapers.push( NewsScrapers::WashPoScraper.new )
     scrapers.push( NewsScrapers::ChicagoTribuneScraper.new )
     scrapers.push( NewsScrapers::LaTimesScraper.new )
     scrapers.push( NewsScrapers::NewYorkTimesScraper.new )
+    self.scrape(self.all_dates, scrapers)
+  end
+  
+  def self.scrape(dates,scrapers)
     dates.each do |d|
-      NewsScrapers.logger.info"  #{d}"
       scrapers.each do |scraper|
+        NewsScrapers.logger.info"  Scraping #{d} from the #{scraper.get_source_name}"
         scraper.scrape(d)
       end
     end
   end
   
+  # TODO: replace with sprintf (this is dumb, but was quick and easy)
   def self.prefix_with_zero number
     fixed = number
     fixed = "0" + number.to_s if number < 10
     fixed
   end
   
+  # Have the cache here at the top level so it is shared across all scraping
   def self.cache
     if @@cache == nil
       @@cache = NewsScrapers::WebpageCache.new( File.join("/tmp","scraper") )
--- lib/tasks/scraper.rake
+++ lib/tasks/scraper.rake
@@ -5,8 +5,24 @@ namespace :scraper do
 	desc "Control the newspaper article scraper"
 
   task :all =>:environment do
-    Rails.logger.info "Starting to scrape all --------------------------------"
     NewsScrapers::scrape_all
   end
+
+  task :washpo =>:environment do
+    NewsScrapers::scrape_washington_post
+  end
+
+  task :chictrib =>:environment do
+    NewsScrapers::scrape_chicago_tribune
+  end
+
+  task :latimes =>:environment do
+    NewsScrapers::scrape_los_angeles_times
+  end
+
+  task :nytimes =>:environment do
+    NewsScrapers::scrape_new_york_times
+  end
+
 
 end
