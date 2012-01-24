ef1c5e1be2fcfe51a0b46cd73e31ff51822efadb
Rahul Bhargava <rahulb@media.mit.edu>
2012-01-23 21:18:21 -0500

    try to autoflush logger in production - hope this doesn't break things!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,8 @@ UsWorldCoverage::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # tell the logger to autoflush
+  Rails.logger.auto_flushing = 1
+  
 end
