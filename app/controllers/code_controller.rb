731b3791e037b102559b7cc96f503ed3a7f85a53
J. nathan Matias <natematias@gmail.com>
2012-02-15 13:00:00 -0500

    refactored code controller to take advantage of new answer creation approach

--- app/controllers/code_controller.rb
+++ app/controllers/code_controller.rb
@@ -16,13 +16,13 @@ class CodeController < ApplicationController
     @user = User.find_by_username(session[:username])
     return if @user.nil? or params[:answer_type].nil?
     return if !(["yes", "no"].include? params[:answer])
-    
+
+    answer = nil
+    answer = 1 if params[:answer] == "yes"
+    answer = 0 if params[:answer] == "no"
+
     article = Article.find_by_id(params[:id]) 
-    @answer = Answer.new_by_type(params[:answer_type])
-    @answer.user = @user
-    @answer.article = article
-    @answer.answer = 1 if params[:answer] == "yes"
-    @answer.answer = 0 if params[:answer] == "no"
+    @answer = Answer.new_by_type(params[:answer_type], {:user=>@user, :article=>article, :source=>"MediaMeter Coder", :answer=>answer})
     @answer.save
 
     #TODO finish while conscious
