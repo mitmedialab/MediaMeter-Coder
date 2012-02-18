22bbc372c07568428b36cce04b4520f87d8a1fe8
J. nathan Matias <natematias@gmail.com>
2012-02-18 16:56:30 -0500

    optimised the method which finds the next unanswered question

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,26 @@ class User < ActiveRecord::Base
  
   def get_next_unanswered_article
     next_unanswered_article = nil
-    Article.find() do |article|
-      if !answers.detect{|answer| answer.article == article}
-        next_unanswered_article = article
-        break
-      end
-    end
+    query_string = ""
+    query_string << <<-SQL
+SELECT articles.*, 
+       selected_answers.user_id 
+  FROM articles 
+  LEFT OUTER JOIN (
+    SELECT * from answers WHERE answers.user_id = #{id}) 
+    AS selected_answers 
+    ON articles.id = selected_answers.article_id 
+  WHERE selected_answers.user_id IS NULL 
+  LIMIT 1;
+SQL
+    result = Article.find_by_sql(query_string)
+    next_unanswered_article = result[0] if result.size > 0
+#    Article.find() do |article|
+#      if !answers.detect{|answer| answer.article == article}
+#        next_unanswered_article = article
+#        break
+#      end
+#    end
     next_unanswered_article
   end
 end
