12a2627c35454f56b307222b131fb236f5fb5a02
natematias <natematias@gmail.com>
2012-02-28 17:14:38 -0500

    added link to pdf for articles with pdfs

--- db/migrate/20120218055848_add_sampletag_to_articles.rb
+++ db/migrate/20120218055848_add_sampletag_to_articles.rb
@@ -1,5 +1,5 @@
 class AddSampletagToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :sampletag, :string
+    add_column :articles, :sampletag, :string, :default=>nil
   end
 end
