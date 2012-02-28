dab4291291be08f410d2c23fe79a6b27986378ce
natematias <natematias@gmail.com>
2012-02-28 17:26:48 -0500

    updated blacklist tool. Coder now selects from sample

--- utilities/blacklist_duplicate_entries.rb
+++ utilities/blacklist_duplicate_entries.rb
@@ -2,15 +2,23 @@ require '../config/environment.rb'
 
 @duplicate_count = 0
 @previous_duplicate_string = nil
-Article.all(:conditions=>['blacklist_tag IS NULL or blacklist_tag="" or blacklist_tag ="duplicate"'], :order=>"pub_date asc, source, headline, abstract").each do |article|
-  duplicate_string = article.headline.to_s + article.source.to_s + article.pub_date.to_s #+ article.abstract.to_s
+@previous_abstract = nil
+Article.all(:conditions=>['(blacklist_tag IS NULL or blacklist_tag="" or blacklist_tag ="duplicate")'], :order=>"source, pub_date asc, headline asc, abstract").each do |article|
+  duplicate_string = "" + article.headline.to_s[0..40] + article.source.to_s + article.pub_date.to_s + article.abstract.to_s[0..10]
   if duplicate_string == @previous_duplicate_string
     article.blacklist_tag = "duplicate"
     article.save
+#    if @previous_abstract != article.abstract
+#      puts 
+#      puts @previous_abstract
+#      puts article.abstract
+#    end
     print "x"
     @duplicate_count += 1
+  else if 
   end
   @previous_duplicate_string = duplicate_string
+  @previous_abstract = article.abstract
 end
 
 puts "Duplicate Count: #{@duplicate_count}"
