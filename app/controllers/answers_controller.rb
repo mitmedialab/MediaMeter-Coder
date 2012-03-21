93f142b2dac75b408ab7015597b90542679cb194
Rahul Bhargava <rahulb@media.mit.edu>
2012-03-21 15:13:06 -0400

    added jquery plugin for stick table header on answers table, to make gold reviewing easier

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -31,7 +31,7 @@ class AnswersController < ApplicationController
     
     # load all the articles
     @articles = Article.where(:sampletag=>@sampletag).includes([:answers,:golds]).
-      where('answers.user_id'=>user_ids)
+      where('answers.user_id'=>user_ids).limit(10)
 
     # compute agreement
     @disagreement_count = 0
