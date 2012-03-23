8a65487e9e6d6d2ed821160e061ccd069aa9146a
Rahul Bhargava <rahulb@media.mit.edu>
2012-03-23 17:23:44 -0400

    major refactoring of coding UI to be jquery based, and live in with the rest of the UI - now it doesn't make my brain hurt

--- app/controllers/code_controller.rb
+++ app/controllers/code_controller.rb
@@ -1,37 +1,55 @@
 class CodeController < ApplicationController
-  layout nil
-  layout "application", :except=>[:answer]
+  layout "browse", :except=>[:answer]
   before_filter :require_username
  
+  def arts
+    @answer_type = "arts"
+    render :code
+  end 
+ 
   def international
+    @answer_type = "international"
+    render :code
   end
 
   def foreign
+    @answer_type = "foreign"
+    render :code
   end
   
   def national
+    @answer_type = "national"
+    render :code
   end
   
   def sports
+    @answer_type = "sports"
+    render :code
+  end
+
+  def local
+    @answer_type = "local"
+    render :code
   end
  
   def answer 
-    return if params[:answer_type].nil?
+    @article = nil
+    @answer_type = nil
+
     @answer_type = params[:answer_type]
     if(params[:answer].nil? or params[:id].nil?)
       @article = @user.get_next_unanswered_article(params[:answer_type])
-      return
+    else
+      answer = nil
+      answer = 1 if params[:answer] == "yes"
+      answer = 0 if params[:answer] == "no"
+      article = Article.find_by_id(params[:id]) 
+      @answer = Answer.new_by_type(@answer_type, {:user=>@user, :article=>article, :source=>"MediaMeter Coder", :answer=>answer})
+      @answer.save
+      @article = @user.get_next_unanswered_article(params[:answer_type])
     end
 
-    return if !(["yes", "no"].include? params[:answer])
-
-    answer = nil
-    answer = 1 if params[:answer] == "yes"
-    answer = 0 if params[:answer] == "no"
-
-    article = Article.find_by_id(params[:id]) 
-    @answer = Answer.new_by_type(@answer_type, {:user=>@user, :article=>article, :source=>"MediaMeter Coder", :answer=>answer})
-    @answer.save
-    @article = @user.get_next_unanswered_article(params[:answer_type])
+    render :partial => "answer"
   end
+  
 end
