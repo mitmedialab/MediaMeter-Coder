1858ff09c41368f48a9d83bc4a5d321d7bf2bd04
Rahul Bhargava <rahulb@media.mit.edu>
2012-04-11 14:54:22 -0400

    more gold info on homepage

--- app/models/gold.rb
+++ app/models/gold.rb
@@ -56,15 +56,21 @@ class Gold < ActiveRecord::Base
     (reason!=nil) && (reason.length > 0)
   end
 
-  # return a hash of type=> pct of golds with reasons
+  # return a hash of type=>total, reasoned, pct of golds with reasons
   def self.reasoned_percent_by_type
     with_reasons = Gold.group(:type).where('reason is not null').count
+    logger.info with_reasons
     all = Gold.group(:type).count
     pcts = {}
     all.each do |type_classname,count|
       type_short_name = type_for_classname(type_classname)
-      pcts[type_short_name] = 0.0
-      pcts[type_short_name] = with_reasons[type_classname].to_f / count.to_f if with_reasons.has_key? type_classname   
+      pct = 0.0 
+      pct = with_reasons[type_classname].to_f / count.to_f if with_reasons.has_key? type_classname
+      pcts[type_short_name] = { 
+        :total => all[type_classname],
+        :reasoned => with_reasons[type_classname], 
+        :pct => pct
+        }   
     end
     pcts
   end
