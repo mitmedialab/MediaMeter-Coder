21284b7d4b498ac106bef9e63f5d8706b7bb1b74
Rahul Bhargava <rahulb@media.mit.edu>
2012-07-19 13:41:37 -0400

    added UI to coding web

--- app/assets/javascripts/code.js
+++ app/assets/javascripts/code.js
@@ -17,17 +17,35 @@ function code_handleKeyPress(evt){
 	var unicode=evt.charCode? evt.charCode : evt.keyCode;
     var actualkey=String.fromCharCode(unicode);
     var currentArticle = $('#articles').find('div:first'); 
-    var articleId = currentArticle.attr('id');
-	if(actualkey=="y"){
-		currentArticle.addClass('yes');
-        code_codeArticle(articleId, "yes");
-	} else if(actualkey=="n"){
-		currentArticle.addClass('no');
-		code_codeArticle(articleId, "no");
+    var articleId = currentArticle.find('.article_id').val();
+	var validKey = false;
+	var answerName = null;
+	if(actualkey=="1"){
+		validKey = true;
+		answerName = 'answer_one';
+	} else if(actualkey=="5"){
+		validKey = true;
+		answerName = 'answer_two';
+	} else if(actualkey=="0"){
+		validKey = true;
+		answerName = 'answer_three';
+	} else if(actualkey=="d"){
+		validKey = true;
+		answerName = 'answer_four';
+	} else if(actualkey=="m"){
+		validKey = true;
+		answerName = 'answer_five';
+	}
+	if(validKey) {
+		pickedAnswer = currentArticle.find('#'+answerName); 
+		pickedAnswer.attr('checked',true);
+		code_codeArticle(articleId,pickedAnswer.val());
 	}
 }
 
 function code_codeArticle(articleId, answer){
+	var currentArticle = $('#article_'+articleId);
+	currentArticle.addClass('done');
 	var authenticityToken =$('#authenticity_token').val();
 	var questionId = $('#question_id').val();
 	var dataToSubmit = { id: articleId, question_id: questionId, answer: answer,  
@@ -39,6 +57,7 @@ function code_codeArticle(articleId, answer){
 			$('#articles').prepend(data);
 		}
 	});
+	currentArticle.find('input').attr('disabled',true);	
 }
 
 function code_init() {
--- app/controllers/code_controller.rb
+++ app/controllers/code_controller.rb
@@ -14,11 +14,16 @@ class CodeController < ApplicationController
     if(params[:answer].nil? or params[:id].nil?)
       @article = @user.get_next_unanswered_article(@question.id)
     else
-      answer = nil
-      answer = 1 if params[:answer] == "yes"
-      answer = 0 if params[:answer] == "no"
+      answer = params[:answer]
+
       article = Article.find_by_id(params[:id]) 
-      @answer = Answer.new({:question_id=>@question.id,:user=>@user, :article=>article, :source=>"MediaMeter Coder", :answer=>answer})
+      @answer = Answer.new({
+         :question_id=>@question.id,
+         :user=>@user, 
+         :article=>article, 
+         :source=>"MediaMeter Coder", 
+         :answer=>answer
+      })
       @answer.save
       @article = @user.get_next_unanswered_article(@question.id)
     end
