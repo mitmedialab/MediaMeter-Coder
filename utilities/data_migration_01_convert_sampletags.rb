78a1311b36fa2b82c9800781463d52a2c8fbca0f
Rahul Bhargava <rahulb@media.mit.edu>
2012-09-30 22:25:52 -0400

    removing old boolean-based scripts

--- utilities/data_migration_01_convert_sampletags.rb
+++
@@ -1,9 +0,0 @@
-require '../config/environment.rb'
-
-count = 0
-Article.all(:conditions=>['sampletag="true"']).each do |article|
-  article.sampletag="hand-coded"
-  article.save
-  count +=1
-end
-puts "converted sampletags for #{count} articles"
